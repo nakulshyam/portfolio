4aab17e1e71a4036735a1f2304fe6a9c9e5c0d80
Ripetaurus <lactic.nakul@gmail.com>
2019-08-02 12:38:47 +0530

    add ejs

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express'),
 	app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-	res.send('Home Route');
+	res.render('index');
 });
 
 app.listen(8080, () => {
